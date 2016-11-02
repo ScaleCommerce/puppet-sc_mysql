08b734723f4387e1fc4b129e3ab943a42a9cd474
Andreas Ziethen <az@ziethen.net>
2016-11-02 14:04:34 +0100

    Update init.pp
    add comments

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,46 @@
 # == Class: sc_mysql
 #
-# Module to install MySQL the ScaleCommerce way.
+# Wrapper module for puppetlabs-mysql to install MySQL the ScaleCommerce way.
 #
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
 #
 # [*use_supervisor*]
 #   can be true or false, default is true.
 #   determines if start script should be used with supervisor
 #
+# [*databases*]
+#   array of databases in short form
+#
 # === Examples
 #
-#  class { 'dummy':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  class { 'sc_mysql':
+#    databases => [ 
+#      database1_name => {
+#        user => 'username',
+#        password => 'password',
+#        host => 'host',
+#        ...
+#      }, 
+#      database2_name => {
+#        ...
+#      }
+#    ],
 #  }
+#
+# hiera example:
+# sc_mysql::databases:
+#   database1_name:
+#     user:     'user'
+#     password: 'password'
+#     host:     'host'
+#     grant:    ['All']
+#     charset:  'utf8'
+#     collate:  'utf8_general_ci'
 #
 # === Authors
 #
-# ScaleCommerce <info@scale.sc>
+# Joscha Krug <jk@scale.sc>
+# Andreas Ziethen <az@scale.sc>
 #
 # === Copyright
 #
