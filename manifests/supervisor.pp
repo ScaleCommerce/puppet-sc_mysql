c69f33e813d56edd2b2d09c5f9e2a4d718f2a960
Thomas Lohner <tl@scale.sc>
2019-04-29 17:42:40 +0200

    use supervisord::program instead of file resource

--- manifests/supervisor.pp
+++ manifests/supervisor.pp
@@ -5,15 +5,20 @@ class sc_mysql::supervisor(
   include sc_supervisor
 
   file { '/etc/init.d/mysql':
-    ensure => link,
-    target => "${sc_supervisor::init_path}/supervisor-init-wrapper",
+    ensure => absent,
   }
 
-  file { "${supervisord::config_include}/mysql.conf":
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template("${module_name}/mysql.supervisor.conf.erb"),
-    notify  => Class[supervisord::reload],
+  supervisord::program { 'mysql':
+    command     => '/bin/bash -c "mkdir -p /var/run/mysqld && chown mysql /var/run/mysqld && exec /usr/sbin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql --skip-log-error --socket=/var/run/mysqld/mysqld.sock --port=3306"',
+    autostart   => true,
+    autorestart => true,
+    require     => Class['Mysql::Server::Config'],
+    before      => Service['mysql'],
   }
+
+  # override default service provider
+  Service <| title == "mysql"|> {
+    provider => supervisor,
+  }
+
 }
