fee3175324ae6ab62280a6807730bb12640c0e86
Andreas Ziethen <az@scale.sc>
2019-05-02 17:37:08 +0200

    update for puppet5

--- test/install.sh
+++ test/install.sh
@@ -5,25 +5,11 @@ sed -i -e "s/nodaemon=true/nodaemon=false/" /etc/supervisord.conf
 echo "Running in $(pwd)"
 echo "Puppet Version: $(puppet -V)"
 
-# configure puppet
-ln -sf $(pwd)/test/hiera.yaml $(puppet config print confdir |cut -d: -f1)/
-curl -s https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf > $(puppet config print confdir |cut -d: -f1)/puppet.conf
-ln -sf $(pwd)/test/hieradata $(puppet config print confdir |cut -d: -f1)/hieradata
-puppet config set certname puppet-test.scalecommerce
-
-apt-get update
-apt-get install apt-transport-https
 puppet module install ajcrowe-supervisord
 puppet module install yo61-logrotate
-puppet module install puppetlabs-mysql
-puppet module install puppetlabs-stdlib
-puppet module install puppetlabs-apt --version 2.4.0
-git clone http://github.com/ScaleCommerce/puppet-sc_supervisor.git /etc/puppet/modules/sc_supervisor
-
-curl -s https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec -v 3.9.3
-
-#ln -sf /builds/sc-puppet/puppet-sc_mysql/test/hiera.yaml /etc/puppet/
-#ln -sf /builds/sc-puppet/puppet-sc_mysql/test/hiera /var/lib/hiera
-#ln -sf /builds/sc-puppet/puppet-sc_mysql/ /etc/puppet/modules/sc_mysql
-#curl -s https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf > /etc/puppet/puppet.conf
-#puppet config set certname puppet-test.scalecommerce
+git clone https://github.com/ScaleCommerce/puppet-supervisor_provider.git $(puppet config print modulepath |cut -d: -f1)/supervisor_provider
+curl -s https://omnitruck.chef.io/install.sh | bash -s -- -P inspec -v 3.9.3
+ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera.yaml $(puppet config print confdir |cut -d: -f1)/
+ln -sf /builds/sc-puppet/puppet-sc_nginx/ $(puppet config print modulepath |cut -d: -f1)/sc_nginx
+curl -s https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf > $(puppet config print confdir |cut -d: -f1)/puppet.conf
+puppet config set certname puppet-test.scalecommerce
