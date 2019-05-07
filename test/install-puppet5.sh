793219cde94ab5f3f8663a853471018a1f3cda8f
Adrian Kirchner <mail@adrian-kirchner.com>
2019-05-07 14:25:32 +0200

    Test-Installation: add puppet deb cleanup

--- test/install-puppet5.sh
+++ test/install-puppet5.sh
@@ -3,6 +3,7 @@ export PATH=/opt/puppetlabs/bin:$PATH
 apt-get purge -y "puppet*"
 wget https://apt.puppetlabs.com/puppet5-release-$(lsb_release -cs).deb
 dpkg -i puppet5-release-$(lsb_release -cs).deb
+rm puppet5-release-$(lsb_release -cs).deb
 apt-get update
 apt-get -y install --no-install-recommends puppet-agent
 ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
