a1d275afb652814fc4122cb5917c5f40f73302fd
Adrian Kirchner <mail@adrian-kirchner.com>
2019-05-07 16:45:02 +0200

    Test MySQL version

--- test/inspec/sc_mysql.rb
+++ test/inspec/sc_mysql.rb
@@ -9,3 +9,7 @@ describe liveUser.query('SHOW DATABASES') do
   its('stdout') { should include 'live_database' }
   its('stdout') { should_not include 'stage_database' }
 end
+
+describe liveUser.query('SELECT VERSION()') do
+  its('stdout') { should match /^5.6/ }
+end
